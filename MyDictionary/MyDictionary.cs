642a8bf2177538e218510c57ec7365617f08f6d0
BERKDEM <BERKDEM@DESKTOP-ARK0ODB>
2022-10-16 18:09:29 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] things;
+
+        public MyDictionary()
+        {
+            things = new T[0];
+        }
+
+        public void Add(T thing)
+        {
+            T[] tempArray = things; 
+            things = new T[things.Length + 1];
+
+            for (int i = 0; i < things.Length; i++)
+            {
+                things[i] = tempArray[i];
+            }
+
+            things[things.Length - 1] = thing;
+        }
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dictionary = new MyDictionary<string>();
+            dictionary.Add("Berk");
+        }
+    }
+}
